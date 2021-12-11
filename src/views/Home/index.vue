ff6cfe21e3bdd3a569b7a3f833b190bc480ef55b
Douglas Vinicius <douglas-cprdev@hotmail.com>
2021-12-11 19:52:19 -0300

    (feat): create home page, create modal factory

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,47 @@
 <template>
-  <h1 class="font-black text-brand-main">Home</h1>
+  <Custom-header @create-account="handleCreateAccount" @login="handleLogin" />
+  <Contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+  </div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+import useModal from '../../hooks/useModal'
+
 export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+    const modal = useModal()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+      modal.open({
+        component: 'ModalLogin'
+      })
+    }
+
+    function handleCreateAccount () {
+      modal.open({
+        component: 'ModalCreateAccount'
+      })
+    }
+
+    return {
+      handleLogin,
+      handleCreateAccount
+    }
+  }
 }
 </script>
