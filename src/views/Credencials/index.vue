6dd9092dc7908fa145ce009d7260466e7eb23cc4
Douglas Vinicius <douglas-cprdev@hotmail.com>
2021-12-13 20:25:07 -0300

    (feat) create content loader component

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -19,10 +19,17 @@
       <p class="mt-10 text-lg text-gray-800 font-regular">
         Este aqui é a sua chave de api
       </p>
-
-      <div class="flex py-3 pl-5 pr-20 mt-2 rounded items-center bg-brand-gray">
+      <content-loader
+        v-if="store.Global.isLoading || isLoading"
+        class="rounder"
+        width="600px"
+        height="50px"
+      />
+      <div
+        v-else
+        class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray">
         <span>{{ store.User.currentUser.apiKey }}</span>
-        <div class="flex ml-20 mr-1">
+        <div class="flex ml-20 mr-5">
           <Icon
             name="copy"
             :color="brandColors.graydark"
@@ -42,7 +49,15 @@
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
 
+      <content-loader
+        v-if="store.Global.isLoading || isLoading"
+        class="rounder"
+        width="600px"
+        height="50px"
+      />
+
       <div
+        v-else
         class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full overflow-x-scroll"
       >
         <pre>&lt;script src="https://douglas-cpr-feedbacker-widget.netlify.app?api_key={{store.User.currentUser.apiKey}}"&gt;&lt;/script&gt;</pre>
@@ -54,16 +69,22 @@
 
 <script>
 import HeaderLogged from '@/components/HeaderLogged'
+import ContentLoader from '@/components/ContentLoader'
 import Icon from '@/components/Icon'
 import useStore from '@/hooks/useStore'
 import palette from '../../../palette'
+import { toRefs, reactive } from 'vue'
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { HeaderLogged, Icon, ContentLoader },
   setup () {
     const store = useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      ...toRefs(state),
       store,
       brandColors: palette.brand
     }
