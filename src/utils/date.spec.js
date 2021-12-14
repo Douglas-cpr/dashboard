83eab5dafea288a90ea6f7de28600197018b47e5
Douglas Vinicius <douglas-cprdev@hotmail.com>
2021-12-14 14:49:56 -0300

    (tests) write tests for utils date

---
+++ src/utils/date.spec.js
@@ -0,0 +1,17 @@
+import {
+  getDiffTimeBetweenCurrentDate
+} from './date'
+
+describe('Date utils', () => {
+  it('should return null when pass a null param', () => {
+    expect(getDiffTimeBetweenCurrentDate(null)).toBe(null)
+  })
+
+  it('should return empty string when pass an undefined param', () => {
+    expect(getDiffTimeBetweenCurrentDate(undefined)).toBe('')
+  })
+
+  it('should return a date when pass a correct param', () => {
+    expect(getDiffTimeBetweenCurrentDate('10-01-2021', new Date('12-01-2021'))).toBe('61 dias atrás')
+  })
+})
